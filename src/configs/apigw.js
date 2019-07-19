5363b011fcd833658f00ad9fd13518c4af73d70c
aroop <aroop@netopss-MacBook-Pro-5.local>
2019-07-19 17:05:55 +0530

    local configs added

--- src/configs/apigw.js
+++ src/configs/apigw.js
@@ -1,17 +1,15 @@
 const configs = {
-    // BASE_URL: 'http://localhost:8080/',
     BASE_URL: 'http://nlp-nmt-160078446.us-west-2.elb.amazonaws.com/app/',
     AUTH_ENDPOINT: 'oauth2/authorize',
     LOGOUT_ENDPOINT: 'logout',
-    // POST_LOGOUT_URL:'returnTo=http://localhost:3000',
-    POST_LOGOUT_URL:'returnTo=http://nlp-nmt-160078446.us-west-2.elb.amazonaws.com',
-    // RETURN_TO:'returnTo=http://localhost:3000/callback',
-    RETURN_TO:'returnTo=http://nlp-nmt-160078446.us-west-2.elb.amazonaws.com/callback',
+    POST_LOGOUT_URL:'returnTo='+window.location.protocol + '//' + window.location.hostname + (window.location.port ? (':' + window.location.port) : ''),
+    RETURN_TO:'returnTo='+window.location.protocol + '//' + window.location.hostname + (window.location.port ? (':' + window.location.port) : '')+'/callback',
     RESPONSE_TYPE: 'response_type=token',
-    // CLIENT_ID: 'client_id=687b1bcf-882a-463b-86c9-3fae03f9b76b',
-    CLIENT_ID: 'client_id=fc7ed37f-546a-41d3-b0f4-96e6ab8274bf',
-    // REDIRECT_URI: 'redirect_uri=http://localhost:3000/callback',
-    REDIRECT_URI: 'redirect_uri=http://nlp-nmt-160078446.us-west-2.elb.amazonaws.com/callback',
+    //Test
+    CLIENT_ID: 'client_id=6d810055-185e-4799-baba-c78693da6134',
+    //Prod
+    // CLIENT_ID: 'client_id=fc7ed37f-546a-41d3-b0f4-96e6ab8274bf',
+    REDIRECT_URI: 'redirect_uri='+window.location.protocol + '//' + window.location.hostname + (window.location.port ? (':' + window.location.port) : '')+'/callback',
     
 };
 
