3f8d76947f34619484f19f9de058d5191a389ab7
aroop <aroop@netopss-MacBook-Pro-5.local>
2019-07-25 15:20:08 +0530

    .

--- src/configs/apigw.js
+++ src/configs/apigw.js
@@ -8,7 +8,7 @@ const configs = {
     //Test
     // CLIENT_ID: 'client_id=6d810055-185e-4799-baba-c78693da6134',
     //Prod
-    CLIENT_ID: 'client_id=fc7ed37f-546a-41d3-b0f4-96e6ab8274bf&scope=view',
+    CLIENT_ID: 'client_id=fc7ed37f-546a-41d3-b0f4-96e6ab8274bf',
     REDIRECT_URI: 'redirect_uri='+window.location.protocol + '//' + window.location.hostname + (window.location.port ? (':' + window.location.port) : '')+'/callback',
     
 };
