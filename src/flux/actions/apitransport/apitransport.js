1183ab57fda6a02983aa392146dc4fef33843ef7
aroop <aroop@netopss-MacBook-Pro-5.local>
2019-07-26 16:53:30 +0530

    unauthrized added

--- src/flux/actions/apitransport/apitransport.js
+++ src/flux/actions/apitransport/apitransport.js
@@ -9,14 +9,15 @@ function dispatchAPIAsync(api) {
   };
 }
 
-function apiStatusAsync(progress, errors, message, res = null) {
+function apiStatusAsync(progress, errors, message, res = null, unauthrized = false) {
   if (res === null || !(res.status && res.status.statusCode && res.status.statusCode !== 200 && res.status.statusCode !== 201)) {
     return {
       type: C.APISTATUS,
       payload: {
         progress,
         error: errors,
-        message: res && res.status && res.status.statusMessage ? res.status.statusMessage : message
+        message: res && res.status && res.status.statusMessage ? res.status.statusMessage : message,
+        unauthrized: unauthrized
       }
     };
   }
@@ -25,7 +26,8 @@ function apiStatusAsync(progress, errors, message, res = null) {
     payload: {
       progress,
       error: res.status.statusCode === 200 || res.status.statusCode === 201,
-      message: res.status.statusCode === 200 || res.status.statusCode === 201 ? message : res.status.errorMessage
+      message: res.status.statusCode === 200 || res.status.statusCode === 201 ? message : res.status.errorMessage,
+      unauthrized: unauthrized
     }
   };
 }
@@ -42,10 +44,11 @@ function success(res, api, dispatch) {
 
 function error(err, api, dispatch) {
   let errorMsg = err.response && (Strings.error.message.http[err.response.status] || Strings.error.message.http.default);
+  console.log(err.response.status)
   if (api.errorMsg || api.errorMsg === null) {
     errorMsg = api.errorMsg === null ? "" : api.errorMsg;
   }
-  dispatch(apiStatusAsync(false, true, errorMsg));
+  dispatch(apiStatusAsync(false, true, errorMsg,null, err.response.status === 401 ? true : false));
   if (typeof api.processNextErrorStep === "function") {
     api.processNextErrorStep();
   }
