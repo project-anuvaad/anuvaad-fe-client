59c17e9f106075703308e42227142433ac2fa11e
aroop <aroop@netopss-MacBook-Pro-5.local>
2019-10-29 11:29:41 +0530

    client id changed

--- src/configs/apigw.js
+++ src/configs/apigw.js
@@ -7,10 +7,8 @@ const configs = {
     RESPONSE_TYPE: 'response_type=token',
     //Test
     // CLIENT_ID: 'client_id='+(process.env.REACT_APP_CLIENT_ID ? process.env.REACT_APP_CLIENT_ID:'ef79a009-444e-4de4-90c4-52a17ec783f9'),
-    //Dev
-    CLIENT_ID: 'client_id=55952c35-8387-4c56-8cf4-a3c1c63714eb',
     //Prod
-    // CLIENT_ID: 'client_id=fc7ed37f-546a-41d3-b0f4-96e6ab8274bf',
+    CLIENT_ID: 'client_id=fc7ed37f-546a-41d3-b0f4-96e6ab8274bf',
     REDIRECT_URI: 'redirect_uri='+window.location.protocol + '//' + window.location.hostname + (window.location.port ? (':' + window.location.port) : '')+'/callback',
     
 };
